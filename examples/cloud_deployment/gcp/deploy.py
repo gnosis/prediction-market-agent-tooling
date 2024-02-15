edab35bc0a73dd1e92129dd404f3d682b1c7ccb8
Peter Jung <peter@jung.ninja>
2024-02-15 13:27:52 +0100

    Fix?

--- examples/cloud_deployment/gcp/deploy.py
+++ examples/cloud_deployment/gcp/deploy.py
@@ -1,20 +1,7 @@
 import getpass
-import random
 
-from prediction_market_agent_tooling.deploy.agent import DeployableAgent
-from prediction_market_agent_tooling.markets.data_models import AgentMarket
+from prediction_market_agent_tooling.deploy.agent_example import DeployableCoinFlipAgent
 from prediction_market_agent_tooling.markets.markets import MarketType
-
-
-class DeployableCoinFlipAgent(DeployableAgent):
-    def pick_markets(self, markets: list[AgentMarket]) -> list[AgentMarket]:
-        if len(markets) > 1:
-            return random.sample(markets, 1)
-        return markets
-
-    def answer_binary_market(self, market: AgentMarket) -> bool:
-        return random.choice([True, False])
-
 
 if __name__ == "__main__":
     agent = DeployableCoinFlipAgent()
---
+++ prediction_market_agent_tooling/deploy/agent_example.py
@@ -0,0 +1,14 @@
+import random
+
+from prediction_market_agent_tooling.deploy.agent import DeployableAgent
+from prediction_market_agent_tooling.markets.data_models import AgentMarket
+
+
+class DeployableCoinFlipAgent(DeployableAgent):
+    def pick_markets(self, markets: list[AgentMarket]) -> list[AgentMarket]:
+        if len(markets) > 1:
+            return random.sample(markets, 1)
+        return markets
+
+    def answer_binary_market(self, market: AgentMarket) -> bool:
+        return random.choice([True, False])

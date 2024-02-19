9169eea3a2c504998a8229da69f25a54c76f46a4
Peter Jung <peter@jung.ninja>
2024-02-19 12:51:44 +0100

    lint

--- examples/cloud_deployment/gcp/deploy.py
+++ examples/cloud_deployment/gcp/deploy.py
@@ -1,10 +1,11 @@
 import getpass
+
 import typer
 
 from prediction_market_agent_tooling.deploy.agent_example import (
     DeployableAgent,
-    DeployableCoinFlipAgent,
     DeployableAlwaysRaiseAgent,
+    DeployableCoinFlipAgent,
 )
 from prediction_market_agent_tooling.markets.markets import MarketType
 

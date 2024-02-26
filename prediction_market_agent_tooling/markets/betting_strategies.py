19ed780ade33a17065dd3f2ac03ebcee26dde234
Peter Jung <peter@jung.ninja>
2024-02-26 15:26:44 +0100

    Option to change deployed agent's bet strategy (#53)

---
+++ prediction_market_agent_tooling/markets/betting_strategies.py
@@ -0,0 +1,12 @@
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket
+
+
+def minimum_bet_to_win(
+    answer: bool, amount_to_win: float, market: AgentMarket
+) -> float:
+    """
+    Estimates the minimum bet amount to win the given amount based on the current market price.
+    """
+    share_price = market.p_yes if answer else market.p_no
+    bet_amount = amount_to_win / (1 / share_price - 1)
+    return bet_amount
--- prediction_market_agent_tooling/markets/manifold/manifold.py
+++ prediction_market_agent_tooling/markets/manifold/manifold.py
@@ -1,8 +1,12 @@
 import typing as t
 from decimal import Decimal
+from math import ceil
 
-from prediction_market_agent_tooling.gtypes import Mana
+from prediction_market_agent_tooling.gtypes import Mana, Probability, mana_type
 from prediction_market_agent_tooling.markets.agent_market import AgentMarket
+from prediction_market_agent_tooling.markets.betting_strategies import (
+    minimum_bet_to_win,
+)
 from prediction_market_agent_tooling.markets.data_models import BetAmount, Currency
 from prediction_market_agent_tooling.markets.manifold.api import (
     get_manifold_binary_markets,
@@ -21,6 +25,10 @@ class ManifoldAgentMarket(AgentMarket):
     def get_tiny_bet_amount(self) -> BetAmount:
         return BetAmount(amount=Decimal(1), currency=self.currency)
 
+    def get_minimum_bet_to_win(self, answer: bool, amount_to_win: float) -> Mana:
+        # Manifold lowest bet is 1 Mana, so we need to ceil the result.
+        return mana_type(ceil(minimum_bet_to_win(answer, amount_to_win, self)))
+
     def place_bet(self, outcome: bool, amount: BetAmount) -> None:
         if amount.currency != self.currency:
             raise ValueError(f"Manifold bets are made in Mana. Got {amount.currency}.")
@@ -36,6 +44,7 @@ class ManifoldAgentMarket(AgentMarket):
             id=model.id,
             question=model.question,
             outcomes=model.outcomes,
+            p_yes=Probability(model.pool.YES),
         )
 
     @staticmethod

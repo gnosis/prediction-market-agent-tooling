53a116da38febdfc2b58b1a2a310f5f87d1e35be
Peter Jung <peter@jung.ninja>
2024-03-05 13:28:09 +0100

    lint

--- prediction_market_agent_tooling/monitor/markets/omen.py
+++ prediction_market_agent_tooling/monitor/markets/omen.py
@@ -8,8 +8,8 @@ from prediction_market_agent_tooling.config import APIKeys
 from prediction_market_agent_tooling.deploy.constants import MARKET_TYPE_KEY
 from prediction_market_agent_tooling.gtypes import ChecksumAddress
 from prediction_market_agent_tooling.markets.data_models import ResolvedBet
-from prediction_market_agent_tooling.markets.omen.omen import get_resolved_omen_bets
 from prediction_market_agent_tooling.markets.markets import MarketType
+from prediction_market_agent_tooling.markets.omen.omen import get_resolved_omen_bets
 from prediction_market_agent_tooling.monitor.monitor import (
     DeployedAgent,
     MonitorSettings,

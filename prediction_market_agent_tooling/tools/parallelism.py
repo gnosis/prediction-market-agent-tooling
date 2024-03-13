6b30c73c0b0a8993930b07c915b31dbe7faed1ef
evangriffiths <egriffiths93@gmail.com>
2024-03-13 15:02:21 +0100

    Fix mypy

--- prediction_market_agent_tooling/tools/parallelism.py
+++ prediction_market_agent_tooling/tools/parallelism.py
@@ -1,18 +1,14 @@
 import concurrent
-import os
 from concurrent.futures import Executor
 from concurrent.futures.thread import ThreadPoolExecutor
 from typing import Callable, TypeVar
-
-max_workers = os.getenv("THREADPOOL_N_THREADS") or None
-THREADPOOL = ThreadPoolExecutor(max_workers=max_workers)
 
 A = TypeVar("A")
 B = TypeVar("B")
 
 
 def par_map(
-    items: list[A], func: Callable[[A], B], executor: Executor = THREADPOOL
+    items: list[A], func: Callable[[A], B], executor: Executor = ThreadPoolExecutor()
 ) -> "list[B]":
     """Applies the function to each element using the specified executor. Awaits for all results.
     If executor is ProcessPoolExecutor, make sure the function passed is pickable, e.g. no lambda functions

d65ef7e9027c950a1e7fff0bfef6ff612ae33dfd
Peter Jung <peter@jung.ninja>
2024-03-18 13:00:54 +0100

    add missing

--- prediction_market_agent_tooling/markets/markets.py
+++ prediction_market_agent_tooling/markets/markets.py
@@ -5,6 +5,9 @@ from prediction_market_agent_tooling.markets.manifold.manifold import (
     ManifoldAgentMarket,
 )
 from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
+from prediction_market_agent_tooling.markets.polymarket.polymarket import (
+    PolymarketAgentMarket,
+)
 
 
 class MarketType(str, Enum):
@@ -16,4 +19,5 @@ class MarketType(str, Enum):
 MARKET_TYPE_TO_AGENT_MARKET: dict[MarketType, type[AgentMarket]] = {
     MarketType.MANIFOLD: ManifoldAgentMarket,
     MarketType.OMEN: OmenAgentMarket,
+    MarketType.POLYMARKET: PolymarketAgentMarket,
 }

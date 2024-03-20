90fca8e3fc95cc07a78ef2358482e74c2cc64b4d
Peter Jung <peter@jung.ninja>
2024-03-20 11:40:28 +0100

    Fix for replication

--- prediction_market_agent_tooling/markets/omen/omen_contracts.py
+++ prediction_market_agent_tooling/markets/omen/omen_contracts.py
@@ -1,4 +1,3 @@
-import json
 import os
 import random
 import typing as t
@@ -400,7 +399,7 @@ class OmenRealitioContract(ContractOnGnosisChain):
         realitio_question = "␟".join(
             [
                 question,
-                json.dumps(outcomes),
+                ",".join(f'"{o}"' for o in outcomes),
                 category,
                 language,
             ]

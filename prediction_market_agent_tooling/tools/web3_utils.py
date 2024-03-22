f058c2d097e779f4020e922bfc2b30e73b1ca028
Peter Jung <peter@jung.ninja>
2024-03-22 09:24:08 +0100

    Add `Invalid` as valid retry for send_function_on_contract_tx

--- prediction_market_agent_tooling/tools/web3_utils.py
+++ prediction_market_agent_tooling/tools/web3_utils.py
@@ -100,7 +100,9 @@ def call_function_on_contract(
 @tenacity.retry(
     # Retry only for the transaction nonce errors (as it can get correct one next time),
     # add other retrieable errors gradually to be safe.
-    retry=tenacity.retry_if_exception_message(match=".*wrong transaction nonce.*"),
+    retry=tenacity.retry_if_exception_message(
+        match="(.*wrong transaction nonce.*)|(.*Invalid.*)"
+    ),
     wait=tenacity.wait_chain(*[tenacity.wait_fixed(n) for n in range(1, 10)]),
 )
 def send_function_on_contract_tx(

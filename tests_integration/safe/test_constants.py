3c65faacb2edefe473c4bdd2a7121da67f4e49a2
Gabriel Fior <fior.gbm@gmail.com>
2024-04-15 17:15:02 -0300

    Added test for create_safe

---
+++ tests_integration/safe/test_constants.py
@@ -0,0 +1,3 @@
+from web3 import Web3
+
+ANVIL_PKEY1 = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
--- tests_integration/safe/test_safe.py
+++ tests_integration/safe/test_safe.py
@@ -1,11 +1,33 @@
+import pytest
+from eth_account import Account
+from gnosis.eth import EthereumClient
+from gnosis.safe import Safe
+
 from prediction_market_agent_tooling.tools.safe import create_safe
+from tests.utils import RUN_PAID_TESTS
+from tests_integration.safe.test_constants import ANVIL_PKEY1
 
 
+@pytest.mark.skipif(not RUN_PAID_TESTS, reason="This test costs money to run.")
 def test_create_safe() -> None:
     # ToDo - Implement this when this issue has been merged (https://github.com/gnosis/prediction-market-agent-tooling/issues/99)
     #  Start local chain (fork Gnosis)
-    #  Call create safe
+    #  Call create safe - for inspiration -> https://github.com/karpatkey/roles_royce/blob/2529d244ed8502d34f9daa9f70fa80e7b1123937/tests/utils.py#L77
     # create_safe(from_private_key=private_key_anvil1)
     #  Assert safe is valid, safe version 1.4.1
     #  Stop local chain
-    assert True
+    ethereum_client = EthereumClient()
+
+    account = Account.from_key(ANVIL_PKEY1)
+    safe_address = create_safe(
+        ethereum_client=ethereum_client,
+        account=account,
+        owners=[account.address],
+        salt_nonce=42,
+        threshold=1,
+    )
+    deployed_safe = Safe(safe_address, ethereum_client)
+    version = deployed_safe.retrieve_version()
+    assert version == "1.4.1"
+    assert ethereum_client.is_contract(safe_address)
+    assert deployed_safe.retrieve_owners() == [account.address]

0cb2bb7db175d1a08a7ef0ac206297810a906622
Gabriel Fior <fior.gbm@gmail.com>
2024-04-22 19:32:33 -0300

    Reactivated test for tx using Safe

--- tests_integration/safe/test_safe.py
+++ tests_integration/safe/test_safe.py
@@ -1,6 +1,3 @@
-import os
-
-import pytest
 from eth_account import Account
 from gnosis.eth import EthereumClient
 from gnosis.safe import Safe
@@ -10,14 +7,12 @@ from web3 import Web3
 from prediction_market_agent_tooling.config import PrivateCredentials
 from prediction_market_agent_tooling.gtypes import xDai
 from prediction_market_agent_tooling.markets.data_models import Currency, TokenAmount
-from prediction_market_agent_tooling.markets.omen.data_models import OMEN_TRUE_OUTCOME
+from prediction_market_agent_tooling.markets.omen.data_models import OMEN_TRUE_OUTCOME, OmenMarket
 from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket, binary_omen_buy_outcome_tx
 from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
     OmenSubgraphHandler,
 )
-from prediction_market_agent_tooling.tools.safe import create_safe
 from prediction_market_agent_tooling.tools.web3_utils import send_xdai_to, xdai_to_wei
-from tests_integration.local_chain_utils import fork_reset_state
 from tests_integration.safe.conftest import print_current_block
 
 
@@ -37,46 +32,24 @@ def test_create_safe(
 
 #@pytest.mark.skip(reason="not yet working on Github CI/CD pipeline")
 def test_send_function_on_contract_tx_using_safe(
-    request: pytest.FixtureRequest,
         local_ethereum_client: EthereumClient,
     local_web3: Web3,
     test_credentials: PrivateCredentials,
+        test_safe: Safe
 ) -> None:
-    # ToDo - Use fixture test_safe
 
-    RPC_URL = os.getenv("GNOSIS_RPC_URL")
-    historical_block = 33579187
-    # port = 8546
-    # web3 = local_web3_at_block(request, historical_block, port)
-    fork_reset_state(
-        local_web3,
-        url=RPC_URL,
-        block=historical_block,
-    )
     print_current_block(local_web3)
-    # Deploy Safe
-    # Deploy safe
-    account = Account.from_key(test_credentials.private_key.get_secret_value())
-    safe_address = create_safe(
-        ethereum_client=local_ethereum_client,
-        account=account,
-        owners=[account.address],
-        salt_nonce=42,
-        threshold=1,
-    )
-    test_safe = Safe(safe_address, local_ethereum_client)
-    # local_ethereum_client = EthereumClient(URI(f"http://localhost:{port}"))
+
+
     print(f"is connected {local_web3.is_connected()} {local_web3.provider}")
-    # local_ethereum_client = EthereumClient(URI(RPC_URL))
     print_current_block(local_web3)
     logger.debug(
         f"provider {local_web3.provider.endpoint_uri} connected {local_web3.is_connected()}"
     )
 
-    # Deploy test_safe
+
     account = Account.from_key(test_credentials.private_key.get_secret_value())
     # Fund safe with xDAI if needed
-    print_current_block(local_web3)
     initial_safe_balance = local_ethereum_client.get_balance(test_safe.address)
     if initial_safe_balance < xdai_to_wei(10):
         send_xdai_to(
@@ -90,11 +63,14 @@ def test_send_function_on_contract_tx_using_safe(
     safe_balance = local_ethereum_client.get_balance(test_safe.address)
     logger.debug(f"safe balance {safe_balance} xDai")
     # Bet on Omen market
-    market_id = Web3.to_checksum_address("0x753d3b31bf1038d5b5aa81015b7b3a6a71e3a6e4")
-    subgraph = OmenSubgraphHandler()
-    omen_market = subgraph.get_omen_market_by_market_id(market_id)
+    # Fetch existing market with enough liquidity
+    min_liquidity_wei = xdai_to_wei(xDai(5))
+    markets = fetch_omen_open_binary_market_with_enough_liquidity(1, min_liquidity_wei)
+    # Check that there is a market with enough liquidity
+    assert len(markets) == 1
+    omen_market = markets[0]
     omen_agent_market = OmenAgentMarket.from_data_model(omen_market)
-    amount = TokenAmount(amount=5, currency=Currency.xDai)
+    amount = TokenAmount(amount=2, currency=Currency.xDai)
     test_credentials.safe_address = test_safe.address
     initial_yes_token_balance = omen_agent_market.get_token_balance(
         test_safe.address, OMEN_TRUE_OUTCOME, web3=local_web3
@@ -124,3 +100,10 @@ def test_send_function_on_contract_tx_using_safe(
     logger.debug(f"final Yes token balance {final_yes_token_balance}")
     print_current_block(local_web3)
     assert initial_yes_token_balance.amount < final_yes_token_balance.amount
+
+def fetch_omen_open_binary_market_with_enough_liquidity(limit=1,
+                                                        liquidity_bigger_than = xdai_to_wei(xDai(5))) -> list[OmenMarket]:
+    return OmenSubgraphHandler().get_omen_binary_markets(limit=limit,
+                                         resolved=False,
+                                         liquidity_bigger_than=liquidity_bigger_than)
+

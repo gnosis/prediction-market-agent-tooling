3a5813ee727aefd0b90fcc02a3974c3170ff5721
Peter Jung <peter@jung.ninja>
2024-04-24 10:26:29 +0200

    Abstract away openai cost calculation and add par_generator

---
+++ prediction_market_agent_tooling/tools/costs.py
@@ -0,0 +1,31 @@
+import typing as t
+from contextlib import contextmanager
+from time import time
+
+from langchain_community.callbacks import get_openai_callback
+from pydantic import BaseModel
+
+from prediction_market_agent_tooling.benchmark.utils import get_llm_api_call_cost
+
+
+class Costs(BaseModel):
+    time: float
+    cost: float
+
+
+@contextmanager
+def openai_costs(model: str) -> t.Generator[Costs, None, None]:
+    costs = Costs(time=0, cost=0)
+    start_time = time()
+
+    with get_openai_callback() as cb:
+        yield costs
+        if cb.total_tokens > 0 and cb.total_cost == 0:
+            # TODO: this is a hack to get the cost for an unsupported model
+            cb.total_cost = get_llm_api_call_cost(
+                model=model,
+                prompt_tokens=cb.prompt_tokens,
+                completion_tokens=cb.completion_tokens,
+            )
+        costs.time = time() - start_time
+        costs.cost = cb.total_cost
--- prediction_market_agent_tooling/tools/parallelism.py
+++ prediction_market_agent_tooling/tools/parallelism.py
@@ -1,7 +1,7 @@
 import concurrent
 from concurrent.futures import Executor
 from concurrent.futures.thread import ThreadPoolExecutor
-from typing import Callable, TypeVar
+from typing import Callable, Generator, TypeVar
 
 # Max workers to 5 to avoid rate limiting on some APIs, create a custom executor if you need more workers.
 DEFAULT_THREADPOOL_EXECUTOR = ThreadPoolExecutor(max_workers=5)
@@ -25,3 +25,18 @@ def par_map(
     for fut in futures:
         results.append(fut.result())
     return results
+
+
+def par_generator(
+    items: list[A],
+    func: Callable[[A], B],
+    executor: Executor = DEFAULT_THREADPOOL_EXECUTOR,
+) -> Generator[B, None, None]:
+    """Applies the function to each element using the specified executor. Yields results as they come.
+    If executor is ProcessPoolExecutor, make sure the function passed is pickable, e.g. no lambda functions.
+    """
+    futures: list[concurrent.futures._base.Future[B]] = [
+        executor.submit(func, item) for item in items
+    ]
+    for fut in concurrent.futures.as_completed(futures):
+        yield fut.result()

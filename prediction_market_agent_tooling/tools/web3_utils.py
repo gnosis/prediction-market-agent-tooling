44c2db30b40660caf24af595c8e629a81e40f1f7
Gabriel Fior <fior.gbm@gmail.com>
2024-04-26 15:21:39 -0300

    Added safe funding to script | added tests (#222)
    * Added safe funding to script | added tests
    
    * Fixed tests
    
    * Implemented PR comments

--- prediction_market_agent_tooling/tools/web3_utils.py
+++ prediction_market_agent_tooling/tools/web3_utils.py
@@ -116,8 +116,20 @@ def prepare_tx(
     function_params: Optional[list[Any] | dict[str, Any]] = None,
     tx_params: Optional[TxParams] = None,
 ) -> TxParams:
+    tx_params_new = _prepare_tx_params(web3, from_address, tx_params)
     contract = web3.eth.contract(address=contract_address, abi=contract_abi)
 
+    # Build the transaction.
+    function_call = contract.functions[function_name](*parse_function_params(function_params))  # type: ignore # TODO: Fix Mypy, as this works just OK.
+    tx_params_new = function_call.build_transaction(tx_params_new)
+    return tx_params_new
+
+
+def _prepare_tx_params(
+    web3: Web3,
+    from_address: ChecksumAddress | None,
+    tx_params: Optional[TxParams] = None,
+) -> TxParams:
     # Fill in required defaults, if not provided.
     tx_params_new = TxParams()
     if tx_params:
@@ -135,9 +147,6 @@ def prepare_tx(
         from_checksummed = Web3.to_checksum_address(tx_params_new["from"])
         tx_params_new["nonce"] = web3.eth.get_transaction_count(from_checksummed)
 
-    # Build the transaction.
-    function_call = contract.functions[function_name](*parse_function_params(function_params))  # type: ignore # TODO: Fix Mypy, as this works just OK.
-    tx_params_new = function_call.build_transaction(tx_params_new)
     return tx_params_new
 
 
@@ -174,16 +183,10 @@ def send_function_on_contract_tx(
         function_params=function_params,
         tx_params=tx_params,
     )
-    # Sign with the private key.
-    signed_tx = web3.eth.account.sign_transaction(
-        tx_params, private_key=from_private_key.get_secret_value()
+
+    receipt_tx = sign_send_and_get_receipt_tx(
+        web3, tx_params, from_private_key, timeout
     )
-    # Send the signed transaction.
-    send_tx = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    # And wait for the receipt.
-    receipt_tx = web3.eth.wait_for_transaction_receipt(send_tx, timeout=timeout)
-    # Verify it didn't fail.
-    check_tx_receipt(receipt_tx)
     return receipt_tx
 
 
@@ -234,4 +237,53 @@ def send_function_on_contract_tx_using_safe(
     tx_hash, tx = safe_tx.execute(from_private_key.get_secret_value())
     receipt_tx = web3.eth.wait_for_transaction_receipt(tx_hash, timeout=timeout)
     check_tx_receipt(receipt_tx)
+    return receipt_tx
+
+
+def sign_send_and_get_receipt_tx(
+    web3: Web3,
+    tx_params_new: TxParams,
+    from_private_key: PrivateKey,
+    timeout: int = 180,
+) -> TxReceipt:
+    # Sign with the private key.
+    signed_tx = web3.eth.account.sign_transaction(
+        tx_params_new, private_key=from_private_key.get_secret_value()
+    )
+    # Send the signed transaction.
+    send_tx = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    # And wait for the receipt.
+    receipt_tx = web3.eth.wait_for_transaction_receipt(send_tx, timeout=timeout)
+    # Verify it didn't fail.
+    check_tx_receipt(receipt_tx)
+    return receipt_tx
+
+
+def send_xdai_to(
+    web3: Web3,
+    from_private_key: PrivateKey,
+    to_address: ChecksumAddress,
+    value: Wei,
+    tx_params: Optional[TxParams] = None,
+    timeout: int = 180,
+) -> TxReceipt:
+    from_address = private_key_to_public_key(from_private_key)
+
+    tx_params_new: TxParams = {"value": value, "to": to_address}
+    if tx_params:
+        tx_params_new.update(tx_params)
+    tx_params_new = _prepare_tx_params(web3, from_address, tx_params_new)
+
+    # We need gas and gasPrice here (and not elsewhere) because we are not calling
+    # contract.functions.myFunction().build_transaction, which autofills some params
+    # with defaults, incl. gas and gasPrice.
+    gas = web3.eth.estimate_gas(tx_params_new)
+    tx_params_new["gas"] = int(
+        gas * 1.5
+    )  # We conservatively overestimate gas here, knowing it will be returned if unused
+    tx_params_new["gasPrice"] = web3.eth.gas_price
+
+    receipt_tx = sign_send_and_get_receipt_tx(
+        web3, tx_params_new, from_private_key, timeout
+    )
     return receipt_tx
--- tests_integration/markets/omen/test_local_chain.py
+++ tests_integration/markets/omen/test_local_chain.py
@@ -1,14 +1,20 @@
 import pytest
+from eth_account import Account
+from numpy import isclose
+from pydantic import SecretStr
 from web3 import Web3
+from web3.types import Wei
 
-from prediction_market_agent_tooling.gtypes import xDai
+from prediction_market_agent_tooling.config import PrivateCredentials
+from prediction_market_agent_tooling.gtypes import PrivateKey, xDai
 from prediction_market_agent_tooling.tools.balances import get_balances
-from prediction_market_agent_tooling.tools.web3_utils import xdai_to_wei
-from tests_integration.conftest import local_web3_at_block
-from tests_integration.local_chain_utils import (
-    get_anvil_test_accounts,
-    send_xdai_to_for_tests,
+from prediction_market_agent_tooling.tools.web3_utils import (
+    send_xdai_to,
+    wei_to_xdai,
+    xdai_to_wei,
 )
+from tests_integration.conftest import local_web3_at_block
+from tests_integration.local_chain_utils import get_anvil_test_accounts
 
 
 def test_connect_local_chain(local_web3: Web3) -> None:
@@ -31,12 +37,13 @@ def test_send_xdai(local_web3: Web3) -> None:
     initial_balance_from = get_balances(from_account.address, local_web3)
     initial_balance_to = get_balances(to_account.address, local_web3)
 
-    send_xdai_to_for_tests(
-        from_address=from_account.address,
+    send_xdai_to(
+        web3=local_web3,
+        from_private_key=PrivateKey(SecretStr(from_account.key.hex())),
         to_address=to_account.address,
         value=value,
-        web3=local_web3,
     )
+
     final_balance_from = get_balances(from_account.address, local_web3)
     final_balance_to = get_balances(to_account.address, local_web3)
     assert int(final_balance_to.xdai - initial_balance_to.xdai) == local_web3.from_wei(
@@ -45,3 +52,36 @@ def test_send_xdai(local_web3: Web3) -> None:
     assert int(
         initial_balance_from.xdai - final_balance_from.xdai
     ) == local_web3.from_wei(value, "ether")
+
+
+def test_send_xdai_from_locked_account(
+    local_web3: Web3,
+    test_credentials: PrivateCredentials,
+):
+    from_account = Account.from_key(test_credentials.private_key.get_secret_value())
+    fund_value = xdai_to_wei(xDai(10))
+    transfer_back_value = xdai_to_wei(xDai(5))
+    random_locked_account = local_web3.eth.account.create()
+    # we fund the random account for later sending
+    send_xdai_to(
+        web3=local_web3,
+        from_private_key=PrivateKey(SecretStr(from_account.key.hex())),
+        to_address=random_locked_account.address,
+        value=fund_value,
+    )
+
+    balance_random = get_balances(random_locked_account.address, local_web3)
+    assert xdai_to_wei(balance_random.xdai) == fund_value
+    send_xdai_to(
+        web3=local_web3,
+        from_private_key=PrivateKey(SecretStr(random_locked_account.key.hex())),
+        to_address=from_account.address,
+        value=transfer_back_value,
+    )
+    balance_random = get_balances(random_locked_account.address, local_web3)
+    # We use isclose due to gas costs minimally affecting the balance
+    assert isclose(
+        balance_random.xdai,
+        wei_to_xdai(Wei(fund_value - transfer_back_value)),
+        rtol=0.001,
+    )

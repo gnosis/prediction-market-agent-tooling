ad8619b44a42672cb0c5305f503ef5df4a1916c3
Peter Jung <peter@jung.ninja>
2024-05-06 10:02:08 +0200

    Patch more loggers

--- prediction_market_agent_tooling/loggers.py
+++ prediction_market_agent_tooling/loggers.py
@@ -1,12 +1,11 @@
+import logging
 import sys
 import typing as t
+import warnings
 from enum import Enum
 
 from loguru import logger
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-if t.TYPE_CHECKING:
-    from loguru import Logger
 
 
 class LogFormat(str, Enum):
@@ -22,26 +21,45 @@ class LogConfig(BaseSettings):
     LOG_FORMAT: LogFormat = LogFormat.DEFAULT
 
 
-GCP_LOG_FORMAT = "{level:<.1}{time:MMDD HH:mm:ss.SSSSSS} {process} {name}:{line}] {message} | {extra}"
+GCP_LOG_LOGURU_FORMAT = (
+    "{level:<.1}{time:MMDD HH:mm:ss} {process} {name}:{line}] {message}"
+)
+GCP_LOG_LOGGING_FORMAT, GCP_LOG_FORMAT_LOGGING_DATEFMT = (
+    "%(levelname).1s%(asctime)s %(process)d %(name)s:%(lineno)d] %(message)s"
+), "%m%d %H:%M:%S"
 
 
-def patch_logger(logger: "Logger") -> None:
+def patch_logger() -> None:
     config = LogConfig()
 
     if config.LOG_FORMAT == LogFormat.GCP:
-        format_ = GCP_LOG_FORMAT
+        format_loguru = GCP_LOG_LOGURU_FORMAT
+        format_logging = GCP_LOG_LOGGING_FORMAT
+        datefmt_logging = GCP_LOG_FORMAT_LOGGING_DATEFMT
 
     elif config.LOG_FORMAT == LogFormat.DEFAULT:
-        format_ = None
+        format_loguru, format_logging, datefmt_logging = None, None, None
 
     else:
         raise ValueError(f"Unknown log format: {config.LOG_FORMAT}")
 
-    if format_ is not None:
+    # Use logging module for warnings.
+    logging.captureWarnings(True)
+    # Change warning formatting to a simpler one (no source code in a new line).
+    warnings.formatwarning = simple_warning_format
+
+    # Change built-in logging.
+    if format_logging is not None:
+        logging.basicConfig(
+            level=logging.DEBUG, format=format_logging, datefmt=datefmt_logging
+        )
+
+    # Change loguru.
+    if format_loguru is not None:
         logger.remove()
         logger.add(
             sys.stdout,
-            format=format_,
+            format=format_loguru,
             level="DEBUG",  # Can be the lowest level, higher ones will use by default this one.
             colorize=True,
         )
@@ -49,6 +67,10 @@ def patch_logger(logger: "Logger") -> None:
     logger.info(f"Patched logger for {config.LOG_FORMAT.value} format.")
 
 
+def simple_warning_format(message, category, filename, lineno, line=None):  # type: ignore[no-untyped-def] # Not typed in the standard library neither.
+    return f"{category.__name__}: {message}\n"
+
+
 if not getattr(logger, "_patched", False):
-    patch_logger(logger)
+    patch_logger()
     logger._patched = True  # type: ignore[attr-defined] # Hacky way to store a flag on the logger object, to not patch it multiple times.

59df365c2ae01c293393959a4e3398fdad057886
Evan Griffiths <56087052+evangriffiths@users.noreply.github.com>
2024-05-13 15:54:07 +0100

    Remove PrivateCredentials class, move functionality into APIKeys class (#239)
    * Remove PrivateCredentials class, move functionality into APIKeys class

--- tests_integration/markets/omen/test_reality.py
+++ tests_integration/markets/omen/test_reality.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from web3 import Web3
 
-from prediction_market_agent_tooling.config import PrivateCredentials
+from prediction_market_agent_tooling.config import APIKeys
 from prediction_market_agent_tooling.markets.omen.omen_contracts import (
     Arbitrator,
     OmenRealitioContract,
@@ -10,10 +10,10 @@ from prediction_market_agent_tooling.markets.omen.omen_contracts import (
 from prediction_market_agent_tooling.tools.utils import utcnow
 
 
-def test_ask_question(local_web3: Web3, test_credentials: PrivateCredentials) -> None:
+def test_ask_question(local_web3: Web3, test_keys: APIKeys) -> None:
     realitio_contract = OmenRealitioContract()
     question_id = realitio_contract.askQuestion(
-        private_credentials=test_credentials,
+        api_keys=test_keys,
         question="Will GNO be above $1000 in 2 minutes from now?",
         category="cryptocurrency",
         outcomes=["Yes", "No"],

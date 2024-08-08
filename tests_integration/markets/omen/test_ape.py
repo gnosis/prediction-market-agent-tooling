e0c33e3102b373a4e47c7bb9fbeabb367b484410
gabrielfior <fior.gbm@gmail.com>
2024-08-08 18:20:30 -0300

    WIP

--- tests_integration/markets/omen/test_ape.py
+++ tests_integration/markets/omen/test_ape.py
@@ -1,8 +1,19 @@
+import pytest
 from ape.managers import NetworkManager
+
+
+@pytest.fixture
+def whale(accounts):
+    account = accounts[0]
+    account.balance = "1000 ETH"  # This calls `anvil_setBalance` under-the-hood.
+    return account
 
 
 def test_dummy(networks: NetworkManager):
     print("accounts", networks)
-    with networks.gnosis.mainnet.use_provider("alchemy") as alchemy:
-        print(alchemy.name)
+    with networks.gnosis.mainnet_fork.use_provider("foundry") as provider:
+        print(provider.name)
+
+    # print(networks.provider.getBalance(whale.address))
+
     assert True

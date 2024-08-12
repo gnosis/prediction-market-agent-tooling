83952e1c1d68f6dd47a2efacec07f1a0d6af123e
gabrielfior <fior.gbm@gmail.com>
2024-08-12 17:42:38 -0300

    Add local_web3 to integration test

--- tests_integration/deploy/test_deploy.py
+++ tests_integration/deploy/test_deploy.py
@@ -1,9 +1,11 @@
+from web3 import Web3
+
 from prediction_market_agent_tooling.deploy.agent_example import DeployableCoinFlipAgent
 from prediction_market_agent_tooling.markets.markets import MarketType
 
 
 # Note that this test can trigger redeeming of funds, which costs money to run.
-def test_local_deployment() -> None:
+def test_local_deployment(local_web3: Web3) -> None:
     DeployableCoinFlipAgent(place_bet=False).deploy_local(
         sleep_time=0.001,
         market_type=MarketType.OMEN,

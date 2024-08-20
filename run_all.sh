74da0d178b53b998e68aa6f7035aeee605d07a2f
Gabriel Fior <fior.gbm@gmail.com>
2024-08-20 14:50:51 -0300

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,6 +0,0 @@
-poetry run mypy
-poetry run isort --profile black .
-poetry run isort --profile black .
-git diff --exit-code --quiet || exit 1
-poetry run autoflake --in-place --remove-all-unused-imports --remove-unused-variables --recursive .
-git diff --exit-code --quiet || exit 1

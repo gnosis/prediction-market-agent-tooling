61c25f3a5100741253df8e7b758161a077360417
Peter Jung <peter@jung.ninja>
2024-09-10 13:13:11 +0200

    Fix getting of asset token on Gnosis chain

---
+++ tests/contracts/test_contracts.py
@@ -0,0 +1,9 @@
+from web3 import Web3
+
+from prediction_market_agent_tooling.markets.omen.omen_contracts import sDaiContract
+
+
+def test_sdai_asset_balance_of() -> None:
+    sDaiContract().get_asset_token_balance(
+        Web3.to_checksum_address("0x7d3A0DA18e14CCb63375cdC250E8A8399997816F")
+    ) >= 0

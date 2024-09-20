06c6aa4f53373c73132eb2d5d9f90ee252e90af0
Peter Jung <peter@jung.ninja>
2024-09-20 16:48:36 +0200

    patch loggers in parmaps

--- prediction_market_agent_tooling/tools/parallelism.py
+++ prediction_market_agent_tooling/tools/parallelism.py
@@ -2,6 +2,8 @@ from typing import Callable, Generator, TypeVar
 
 from loky import get_reusable_executor
 
+from prediction_market_agent_tooling.loggers import patch_logger
+
 A = TypeVar("A")
 B = TypeVar("B")
 
@@ -12,7 +14,7 @@ def par_map(
     max_workers: int = 5,
 ) -> "list[B]":
     """Applies the function to each element using the specified executor. Awaits for all results."""
-    executor = get_reusable_executor(max_workers=max_workers)
+    executor = get_reusable_executor(max_workers=max_workers, initializer=patch_logger)
     futures = [executor.submit(func, item) for item in items]
     results = []
     for fut in futures:
@@ -26,6 +28,6 @@ def par_generator(
     max_workers: int = 5,
 ) -> Generator[B, None, None]:
     """Applies the function to each element using the specified executor. Yields results as they come."""
-    executor = get_reusable_executor(max_workers=max_workers)
+    executor = get_reusable_executor(max_workers=max_workers, initializer=patch_logger)
     for res in executor.map(func, items):
         yield res

771f7f875d9b0dee9c6bcced5a930d0a067cbcc3
Peter Jung <peter@jung.ninja>
2024-10-15 10:23:23 +0200

    Fix

--- scripts/remove_liquidity_omen.py
+++ scripts/remove_liquidity_omen.py
@@ -1,27 +1,13 @@
-from datetime import datetime
-
 import typer
 from web3 import Web3
 
 from prediction_market_agent_tooling.config import APIKeys
-from prediction_market_agent_tooling.gtypes import private_key_type, xdai_type
+from prediction_market_agent_tooling.gtypes import private_key_type
 from prediction_market_agent_tooling.loggers import logger
-from prediction_market_agent_tooling.markets.omen.data_models import (
-    OMEN_BINARY_MARKET_OUTCOMES,
-)
-from prediction_market_agent_tooling.markets.omen.omen import (
-    omen_create_market_tx,
-    omen_remove_fund_market_tx,
-)
+from prediction_market_agent_tooling.markets.omen.omen import omen_remove_fund_market_tx
 from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
     OmenSubgraphHandler,
 )
-from prediction_market_agent_tooling.markets.omen.omen_contracts import (
-    COLLATERAL_TOKEN_CHOICE_TO_ADDRESS,
-    OMEN_DEFAULT_MARKET_FEE_PERC,
-    CollateralTokenChoice,
-)
-from prediction_market_agent_tooling.tools.utils import DatetimeUTC
 
 
 def main(
@@ -48,10 +34,8 @@ def main(
     market = OmenSubgraphHandler().get_omen_market_by_market_id(
         Web3.to_checksum_address(market_id)
     )
-    market_address = omen_remove_fund_market_tx(
-        api_keys=api_keys, market=market, shares=None
-    )
-    logger.info(f"Market created at address: {market_address}")
+    omen_remove_fund_market_tx(api_keys=api_keys, market=market, shares=None)
+    logger.info(f"Liquidity removed from: {market_id}")
 
 
 if __name__ == "__main__":

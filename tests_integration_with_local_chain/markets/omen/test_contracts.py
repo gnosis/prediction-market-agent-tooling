f0511ff9ae82860a6111302781105d96e06b39bc
gabrielfior <fior.gbm@gmail.com>
2024-11-07 20:06:01 -0300

    Fixed black

--- tests_integration_with_local_chain/markets/omen/test_contracts.py
+++ tests_integration_with_local_chain/markets/omen/test_contracts.py
@@ -7,7 +7,7 @@ def test_sdai_asset_balance_of(local_web3: Web3) -> None:
     assert (
         sDaiContract().get_asset_token_balance(
             Web3.to_checksum_address("0x7d3A0DA18e14CCb63375cdC250E8A8399997816F"),
-            web3=local_web3
+            web3=local_web3,
         )
         >= 0
     )

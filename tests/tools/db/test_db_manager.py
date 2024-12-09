a057f72b62d8ea504662bbc4417ad0ce0690303b
gabrielfior <fior.gbm@gmail.com>
2024-12-09 12:04:10 -0300

    fix test

--- tests/tools/db/test_db_manager.py
+++ tests/tools/db/test_db_manager.py
@@ -9,7 +9,7 @@ def test_DBManager_creates_only_one_instance() -> None:
     ) as temp_db1, tempfile.NamedTemporaryFile(suffix=".db") as temp_db3:
         db1 = DBManager(f"sqlite:///{temp_db1.name}")
         db2 = DBManager(f"sqlite:///{temp_db1.name}")
-        db3 = DBManager(f"sqlite:///{temp_db1.name}")
+        db3 = DBManager(f"sqlite:///{temp_db3.name}")
         are_same_instance = db1 is db2
         are_not_same_instance = db1 is not db3
 

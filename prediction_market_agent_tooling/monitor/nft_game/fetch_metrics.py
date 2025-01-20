e0449d1d2b5896adb6c1e3aa51ef21e7241e0800
gabrielfior <fior.gbm@gmail.com>
2025-01-20 08:22:25 -0300

    Added balance extraction

--- prediction_market_agent_tooling/monitor/nft_game/fetch_metrics.py
+++ prediction_market_agent_tooling/monitor/nft_game/fetch_metrics.py
@@ -2,12 +2,19 @@ import time
 
 from eth_typing import ChecksumAddress
 from web3 import Web3
+from web3.types import Wei
 
 from prediction_market_agent_tooling.loggers import logger
-from prediction_market_agent_tooling.monitor.nft_game.models import ERC721Transfer
+from prediction_market_agent_tooling.monitor.nft_game.models import (
+    AgentCommunicationMessage,
+    ERC721Transfer,
+    BalanceData,
+)
 from prediction_market_agent_tooling.tools.contract import (
+    AgentCommunicationContract,
     ContractOwnableERC721BaseClass,
 )
+from prediction_market_agent_tooling.tools.parallelism import par_map
 
 
 def fetch_nft_transfers(
@@ -24,16 +31,65 @@ def fetch_nft_transfers(
     logs = nft_c.events.Transfer().get_logs(fromBlock=from_block, toBlock=to_block)
     logger.debug(f"elapsed {time.time() - start}")
     logger.debug(f"fetched {len(logs)} NFT transfers")
-
     events = [ERC721Transfer.from_event_log(log) for log in logs]
     return events
 
 
-def extract_messages_exchanged():
-    # ToDo
-    pass
+def extract_messages_exchanged(
+    web3: Web3,
+    from_block: int = 37341108,
+    to_block: int | None = None,
+) -> list[AgentCommunicationMessage]:
+    agent_communication_contract = AgentCommunicationContract()
+    agent_communication_c = agent_communication_contract.get_web3_contract(web3=web3)
+
+    start = time.time()
+    logs = agent_communication_c.events.LogMessage().get_logs(
+        fromBlock=from_block, toBlock=to_block
+    )
+    logger.debug(f"elapsed {time.time() - start}")
+    logger.debug(f"fetched {len(logs)} events from AgentCommunication contract")
+    messages = [AgentCommunicationMessage.from_event_log(log) for log in logs]
+    return messages
 
 
-def extract_balances_per_block():
-    # ToDo
-    pass
+def get_balance_at_block(
+    web3: Web3, address: ChecksumAddress, block: int | None = None
+) -> tuple[Wei, int]:
+    xdai_balance = Wei(web3.eth.get_balance(account=address, block_identifier=block))
+    return xdai_balance, block
+
+
+def extract_balances_per_block(
+    web3: Web3,
+    from_block: int = 37341108,
+    to_block: int | None = None,
+) -> list[AgentCommunicationMessage]:
+    # ToDo - call get_balance in range(from_block, to_block + 1)
+    blocks = list(range(from_block, to_block + 1))  # include end block
+
+    NFT_agent_addresses = [
+        "0xd845A24014B3BD96212A21f602a4F16A7dA518A4",
+        "0xb4D8C8BedE2E49b08d2A22485f72fA516116FE7F",
+        "0xC09a8aB38A554022ACBACBA174F14C8B35E89946",
+        "0xd4fC4305DC1226c38356024c26cdE985817f137F",
+        "0x84690A78d74e90608fc3e73cA79A06ee4F261A06",
+        "0x64D94C8621128E1C813F8AdcD62c4ED7F89B1Fd6",
+        "0x469Bc26531800068f306D304Ced56641F63ae140",
+    ]
+    NFT_agent_addresses = [Web3.to_checksum_address(i) for i in NFT_agent_addresses]
+
+    balance_data: dict[ChecksumAddress, list[BalanceData]] = {}
+
+    for address in NFT_agent_addresses:
+        balances = par_map(
+            items=blocks,
+            func=lambda block: get_balance_at_block(
+                web3=web3, address=address, block=block
+            ),
+        )
+        balance_data[address] = [
+            BalanceData(address=address, block=b, balance=balance)
+            for balance, b in balances
+        ]
+    print(balance_data)
--- prediction_market_agent_tooling/monitor/nft_game/models.py
+++ prediction_market_agent_tooling/monitor/nft_game/models.py
@@ -3,11 +3,14 @@ from pydantic import BaseModel, Field
 from prediction_market_agent_tooling.tools.hexbytes_custom import HexBytes
 
 
-class ERC721Transfer(BaseModel):
+class EventBase(BaseModel):
     block_number: int = Field(alias="blockNumber")
     transaction_hash: HexBytes = Field(alias="transactionHash")
     address: HexBytes
     event: str
+
+
+class ERC721Transfer(EventBase):
     from_address: HexBytes
     to_address: HexBytes
     token_id: int
@@ -20,3 +23,25 @@ class ERC721Transfer(BaseModel):
             "token_id": log["args"]["tokenId"],
         }
         return ERC721Transfer.model_validate({**d, **log})
+
+
+class AgentCommunicationMessage(EventBase):
+    sender: HexBytes
+    agent_address: HexBytes = Field(alias="agentAddress")
+    message: bytes
+    value: int
+
+    @classmethod
+    def from_event_log(cls, log: dict) -> "AgentCommunicationMessage":
+        d = {
+            "from_address": log["args"]["from"],
+            "to_address": log["args"]["to"],
+            "token_id": log["args"]["tokenId"],
+        }
+        return AgentCommunicationMessage.model_validate({**d, **log})
+
+
+class BalanceData(BaseModel):
+    block: int
+    address: str
+    balance_wei: int

2fd4b86855a457f160d3b23ca03562b61e156fdf
Peter Jung <peter@jung.ninja>
2025-02-04 12:28:16 +0100

    Pass n to find_resolution_on_manifold

--- prediction_market_agent_tooling/markets/manifold/utils.py
+++ prediction_market_agent_tooling/markets/manifold/utils.py
@@ -4,9 +4,10 @@ from prediction_market_agent_tooling.markets.manifold.api import (
 )
 
 
-def find_resolution_on_manifold(question: str) -> Resolution | None:
+def find_resolution_on_manifold(question: str, n: int = 100) -> Resolution | None:
+    # Even with exact-match search, Manifold doesn't return it as the first result, increase `n` if you can't find market that you know exists.
     manifold_markets = get_manifold_binary_markets(
-        10, term=question, filter_=None, sort=None
+        n, term=question, filter_=None, sort=None
     )
     for manifold_market in manifold_markets:
         if manifold_market.question == question:

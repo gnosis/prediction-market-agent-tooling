9ee97ab91479513063a529143195b9e127e5b9be
gabrielfior <fior.gbm@gmail.com>
2025-02-06 18:10:43 -0300

    Contract reverted to old state

--- tests_integration_with_local_chain/nft_agents/test_agent_communication.py
+++ tests_integration_with_local_chain/nft_agents/test_agent_communication.py
@@ -42,9 +42,9 @@ def test_count_unseen_messages(local_web3: Web3, accounts: list[TestAccount]) ->
 def test_pop_message(
     local_web3: Web3, test_keys: APIKeys, accounts: list[TestAccount]
 ) -> None:
-    #### Delete me after test passes
-    # ToDo - Try with new account, assert balance first
-    mock_agent_address = Web3.to_checksum_address(accounts[3].address)
+    # Recipient same as caller since popMessage() will be called afterwards, and it requires
+    # the sender to be the message popper.
+    recipient = test_keys.bet_from_address
     print(
         f"balance {get_balances(address=test_keys.bet_from_address, web3=local_web3)}"
     )
@@ -52,67 +52,35 @@ def test_pop_message(
 
     # It might be the case that initial_messages > 0 (due to ape's tests not being isolated).
     initial_messages = comm_contract.count_unseen_messages(
-        agent_address=mock_agent_address, web3=local_web3
+        agent_address=recipient, web3=local_web3
     )
 
     # add new message
-    message = zlib.compress(b"Hello there!")
+    message = zlib.compress("Hello there!".encode(), level=zlib.Z_BEST_COMPRESSION)
 
     comm_contract.send_message(
         api_keys=test_keys,
-        agent_address=mock_agent_address,
-        message=HexBytes("hello".encode()),
+        agent_address=recipient,
+        message=HexBytes(message),
         amount_wei=xdai_to_wei(xDai(0.1)),
         web3=local_web3,
     )
     assert (
-        comm_contract.count_unseen_messages(
-            agent_address=mock_agent_address, web3=local_web3
-        )
+        comm_contract.count_unseen_messages(agent_address=recipient, web3=local_web3)
         == initial_messages + 1
     )
-    ###
 
-    # keys = APIKeys()
-    # mock_agent_address = keys.bet_from_address
-    # comm_contract = AgentCommunicationContract()
-    #
-    # initial_messages = comm_contract.count_unseen_messages(
-    #     agent_address=mock_agent_address, web3=local_web3
-    # )
-    #
-    # message = zlib.compress(b"Hello there!")
-    # print(f"initial messages {initial_messages}")
-    #
-    # comm_contract.send_message(
-    #     api_keys=keys,
-    #     agent_address=mock_agent_address,
-    #     message=HexBytes(message),
-    #     amount_wei=xdai_to_wei(xDai(0.1)),
-    #     web3=local_web3,
-    # )
-    #
-    # assert (
-    #     comm_contract.count_unseen_messages(
-    #         agent_address=mock_agent_address, web3=local_web3
-    #     )
-    #     == initial_messages + 1
-    # )
-    #
-    # comm_contract.pop_message()
-    # # get at index
-    # stored_message = comm_contract.get_at_index(
-    #     agent_address=mock_agent_address, idx=0, web3=local_web3
-    # )
-    # print(f"stored message {stored_message}")
-    # # assert message match
-    # assert stored_message.recipient == mock_agent_address
-    # assert stored_message.message == HexBytes(message)
-    #
-    # # fetch latest message
-    # stored_message = comm_contract.pop_message(
-    #     keys, mock_agent_address, web3=local_web3
-    # )
-    # # assert message match
-    # assert stored_message.recipient == mock_agent_address
-    # assert stored_message.message == message
+    # get at index
+    stored_message = comm_contract.get_at_index(
+        agent_address=recipient, idx=0, web3=local_web3
+    )
+    print(f"stored message {stored_message}")
+    # assert message match
+    assert stored_message.recipient == recipient
+    assert stored_message.message == HexBytes(message)
+
+    # fetch latest message
+    stored_message = comm_contract.pop_message(test_keys, recipient, web3=local_web3)
+    # assert message match
+    assert stored_message.recipient == recipient
+    assert stored_message.message == message

6e2e8eb8f5aaaa594534d2276c2c67cd41e3f46f
Peter Jung <peter@jung.ninja>
2025-03-21 12:46:09 +0100

    dum

--- prediction_market_agent_tooling/tools/_generic_value.py
+++ prediction_market_agent_tooling/tools/_generic_value.py
@@ -21,6 +21,7 @@ class _GenericValue(
     A helper class intended for inheritance. Do not instantiate this class directly.
 
     Example:
+
     ```python
     a = _GenericValue(10)
     b = Token(100) # Token is a subclass of _GenericValue
@@ -94,14 +95,12 @@ class _GenericValue(
         return type(self)(self.value * (other if isinstance(other, (int, float)) else other.value))  # type: ignore
 
     @overload
-    def __truediv__(self: GenericValueType, other: int | float) -> GenericValueType:
-        ...
+    def __truediv__(self: GenericValueType, other: int | float) -> GenericValueType: ...
 
     @overload
     def __truediv__(
         self: GenericValueType, other: GenericValueType
-    ) -> InternalValueType:
-        ...
+    ) -> InternalValueType: ...
 
     def __truediv__(
         self: GenericValueType, other: GenericValueType | int | float
@@ -118,14 +117,14 @@ class _GenericValue(
             return self.value / other.value  # type: ignore
 
     @overload
-    def __floordiv__(self: GenericValueType, other: int | float) -> GenericValueType:
-        ...
+    def __floordiv__(
+        self: GenericValueType, other: int | float
+    ) -> GenericValueType: ...
 
     @overload
     def __floordiv__(
         self: GenericValueType, other: GenericValueType
-    ) -> InternalValueType:
-        ...
+    ) -> InternalValueType: ...
 
     def __floordiv__(
         self: GenericValueType, other: GenericValueType | int | float

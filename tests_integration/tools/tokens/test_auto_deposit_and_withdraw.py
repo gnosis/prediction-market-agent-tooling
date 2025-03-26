714aaa349393d220a8d8f2e16c2fe013ae97bad6
Peter Jung <peter@jung.ninja>
2025-03-26 10:46:32 +0100

    Fix test test_auto_deposit_and_withdraw

--- tests_integration/tools/tokens/test_auto_deposit_and_withdraw.py
+++ tests_integration/tools/tokens/test_auto_deposit_and_withdraw.py
@@ -50,31 +50,28 @@ def test_auto_deposit_and_withdraw(
         to_deposit,
         KEEPING_ERC20_TOKEN.address,
     )
-    to_deposit_as_keeping_token_wei = to_deposit_as_keeping_token.as_wei
-
     # and in the other token
-    to_deposit_as_token_2 = get_usd_in_token(
+    to_deposit_as_col_token = get_usd_in_token(
         to_deposit,
         collateral_token_contract.address,
     )
-    to_deposit_as_token_2_wei = to_deposit_as_token_2.as_wei
 
     # auto_deposit will deposit only as much as is needed, so add the existing balance to the amount we want to deposit in this test.
-    initial_balance_of_token_2_in_usd = get_token_in_usd(
+    initial_balance_of_col_token_in_usd = get_token_in_usd(
         collateral_token_contract.balance_of_in_tokens(api_keys.bet_from_address),
         collateral_token_contract.address,
     )
-    total_amount_in_usd = initial_balance_of_token_2_in_usd + to_deposit
-    # And how much we expect to have in token_2 after the deposit.
-    total_amount_in_token_2 = get_usd_in_token(
+    total_amount_in_usd = initial_balance_of_col_token_in_usd + to_deposit
+    # And how much we expect to have in col_token after the deposit.
+    total_amount_in_col_token = get_usd_in_token(
         total_amount_in_usd,
         collateral_token_contract.address,
     )
 
-    before_deposit_balance_main_token = KEEPING_ERC20_TOKEN.balanceOf(
+    before_deposit_balance_main_token = KEEPING_ERC20_TOKEN.balance_of_in_tokens(
         api_keys.bet_from_address
     )
-    before_deposit_balance_token_2 = collateral_token_contract.balanceOf(
+    before_deposit_balance_col_token = collateral_token_contract.balance_of_in_tokens(
         api_keys.bet_from_address
     )
 
@@ -84,10 +81,10 @@ def test_auto_deposit_and_withdraw(
         api_keys=api_keys,
     )
 
-    after_deposit_balance_main_token = KEEPING_ERC20_TOKEN.balanceOf(
+    after_deposit_balance_main_token = KEEPING_ERC20_TOKEN.balance_of_in_tokens(
         api_keys.bet_from_address
     )
-    after_deposit_balance_token_2 = collateral_token_contract.balanceOf(
+    after_deposit_balance_col_token = collateral_token_contract.balance_of_in_tokens(
         api_keys.bet_from_address
     )
 
@@ -95,32 +92,34 @@ def test_auto_deposit_and_withdraw(
         after_deposit_balance_main_token <= before_deposit_balance_main_token
     ), "Should be less or equal, because if it was 0, then it should have been deposited and then used which would result in 0. Or, if it was more, it should have be withdrawn from it."
     assert (
-        after_deposit_balance_token_2 > before_deposit_balance_token_2
+        after_deposit_balance_col_token > before_deposit_balance_col_token
     ), "Should be more, because we swapped into it."
     assert after_deposit_balance_main_token == 0 or np.isclose(
         after_deposit_balance_main_token.value,
-        (before_deposit_balance_main_token - to_deposit_as_keeping_token_wei).value,
+        (before_deposit_balance_main_token - to_deposit_as_keeping_token).value,
         rtol=rtol,
     ), "Should be 0 if it was deposited and then used, or should be less by the amount we sold."
     assert np.isclose(
-        after_deposit_balance_token_2.value,
-        (before_deposit_balance_token_2 + to_deposit_as_token_2_wei).value,
+        after_deposit_balance_col_token.value,
+        (before_deposit_balance_col_token + to_deposit_as_col_token).value,
         rtol=rtol,
     ), "Should be more by the amount we bought."
     assert np.isclose(
-        total_amount_in_token_2.value, after_deposit_balance_token_2.value, rtol=rtol
+        total_amount_in_col_token.value,
+        after_deposit_balance_col_token.value,
+        rtol=rtol,
     ), "Should be equal, because we bought exactly as much as we expected in the beginning."
 
     auto_withdraw_collateral_token(
         collateral_token_contract,
-        amount_wei=to_deposit_as_token_2_wei,
+        amount_wei=to_deposit_as_col_token.as_wei,
         api_keys=api_keys,
     )
 
-    after_withdraw_balance_main_token = KEEPING_ERC20_TOKEN.balanceOf(
+    after_withdraw_balance_main_token = KEEPING_ERC20_TOKEN.balance_of_in_tokens(
         api_keys.bet_from_address
     )
-    after_withdraw_balance_token_2 = collateral_token_contract.balanceOf(
+    after_withdraw_balance_col_token = collateral_token_contract.balance_of_in_tokens(
         api_keys.bet_from_address
     )
 
@@ -128,15 +127,15 @@ def test_auto_deposit_and_withdraw(
         after_withdraw_balance_main_token > after_deposit_balance_main_token
     ), "When we withdraw it back, it should be always more then what we had after the deposit."
     assert (
-        after_withdraw_balance_token_2 < after_deposit_balance_token_2
+        after_withdraw_balance_col_token < after_deposit_balance_col_token
     ), "Should be less, because we sold it for the main token."
     assert np.isclose(
         after_withdraw_balance_main_token.value,
-        (after_deposit_balance_main_token + to_deposit_as_keeping_token_wei).value,
+        (after_deposit_balance_main_token + to_deposit_as_keeping_token).value,
         rtol=rtol,
     ), "Should be more by the amount we bought."
     assert np.isclose(
-        after_withdraw_balance_token_2.value,
-        (after_deposit_balance_token_2 - to_deposit_as_token_2_wei).value,
+        after_withdraw_balance_col_token.value,
+        (after_deposit_balance_col_token - to_deposit_as_col_token).value,
         rtol=rtol,
     ), "Should be less by the amount we sold."

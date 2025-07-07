2d7e1eae8dffe3756993d17f8275cd9baa10616a
gabrielfior <fior.gbm@gmail.com>
2025-07-07 13:08:41 +0200

    fix pre-commit

--- tests_integration/markets/polymarket/test_get_conditions_using_subgraph.py
+++ tests_integration/markets/polymarket/test_get_conditions_using_subgraph.py
@@ -9,6 +9,6 @@ def test_get_conditions_using_subgraph() -> None:
     condition_ids = [
         HexBytes("0xfc7c7189f8ca61dab1caa934a4f0fcd8d3ec7b5373564063ca90b5963aacf441"),
         HexBytes("0xb86f59208df00409351d8786283aae7fa8eab197b316cd095e57513b70575681"),
-    ]
+    ]  # web3-private-key-ok
     conditions = s.get_conditions(condition_ids=condition_ids)
     assert len(conditions) == len(condition_ids)
